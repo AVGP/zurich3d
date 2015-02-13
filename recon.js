d5f32ff20eb16c33d499b2e16247d8aab6a5b886
Martin Naumann <martin@geekonaut.de>
2015-02-13 17:08:49 +0100

    Cleaning up recon.js

--- recon.js
+++ recon.js
@@ -1,12 +1,12 @@
 var ps = require('geo-pixel-stream'),
-    readers = ps.createReadStreams('6790_2470.tif');
+    readers = ps.createReadStreams('6830_2470.tif');
 
 console.log(readers);
 console.log(readers.length);
 console.log(readers[0].metadata);
 
 readers[0].on('data', function(data) {
-  console.log("----", data.offset, data.blockSize);
+  console.log("----", data.offset, data.blockSize, data.buffer[0]);
 });
 
 readers[0].on('end', function() {
